0a76a966113082ba1132a31654ac5556fae6d1dc
iZarif <aydar.js@gmail.com>
2021-01-06 19:44:45 +0300

    refactor examples

--- example/slideshow.scm
+++ example/slideshow.scm
@@ -1,16 +1,10 @@
 (import (prefix telebot telebot:)
 	(chicken process-context)
-	(srfi-1)
-	(shell))
+	(chicken process))
 
 (define admin "$user_id")
 (define sorry "I'm sorry Dave, I'm afraid I can't do that.")
 (define greeting "Greetings human. I am a software demon enabling you to control the slideshow displayed on my creator's laptop.")
-
-(define (resolve-query query tree)
-  (fold (lambda (x y) (alist-ref x y))
-        tree
-        query))
 
 (define (make-sender token chat_id)
   (lambda (text)
@@ -24,11 +18,11 @@
     (lambda ()
       (if state
         (begin (set! state #f)
-               (run "geeqie --remote --slideshow-stop")
+               (system "geeqie --remote --slideshow-stop")
                (send "Slideshow stopped."))
         (begin (set! state #t)
-               (run "geeqie --remote --delay=6")
-               (run "geeqie --remote --slideshow-start")
+               (system "geeqie --remote --delay=6")
+               (system "geeqie --remote --slideshow-start")
                (send "Slideshow started."))))))
 
 (define (make-conversation token chat_id)
@@ -37,12 +31,12 @@
          (slideshow (make-slideshow-closure send)))
     (send greeting)
     (lambda (update)
-      (let* ((text  (resolve-query '(message text) update)))
+      (let* ((text  (telebot:resolve-query '(message text) update)))
         (print chat_id " -> \"" text "\"")
         (if (and (string? text)
                  (string=? chat_id admin))
-          (cond ((string=? text "n")     (run "geeqie --remote --next"))
-                ((string=? text "p")     (run "geeqie --remote --back"))
+          (cond ((string=? text "n")     (system "geeqie --remote --next"))
+                ((string=? text "p")     (system "geeqie --remote --back"))
                 ((string=? text "s")     (slideshow))
                 (else                    (send sorry)))
           (send sorry))))))
